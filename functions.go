1161543983235f88aebbbfa982f5c3582062d6e8
zachpuck <zachpuckett@gmail.com>
2017-12-25 22:59:41 -0800

    function

---
+++ functions.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	module := "function basics"
+	author := "zach"
+
+	fmt.Println(converter(module, author))
+}
+
+func converter(module, author string) (s1, s2 string) {
+	module = strings.Title(module)
+	author = strings.ToUpper(author)
+
+	return module, author	
+}
+
