0098f2051080a5eff4ff40d8fba17b0fb2262afd
zachpuck <zachpuckett@gmail.com>
2017-12-27 23:16:45 -0800

    simple loop

---
+++ loops.go
@@ -0,0 +1,22 @@
+// for keyword
+// expression
+// - Blank expression = infinit loop
+// can be Boolean expression
+// can be a range
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for timer := 10; timer >= 0; timer-- {
+		if timer == 0 {
+			fmt.Println("Liftoff!")
+			break
+		}
+		fmt.Println("Countdown", timer)
+		time.Sleep(1 * time.Second)
+	}
+}
