db3e629bbfb11ac3c3d8f4b688ffc4bae743e382
zachpuck <zachpuckett@gmail.com>
2017-12-27 23:33:03 -0800

    working with continue statement

---
+++ loop-continue.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main(){
+	for timer := 10; timer >= 0; timer-- {
+		if timer % 2 == 0 {
+			continue
+		}
+		fmt.Println("Countdown", timer)
+		time.Sleep(1 * time.Second)
+	} 
+}
