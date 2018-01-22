12ad1e48db96a7c5c7ec6663423949ae0ec882b1
zachpuck <zachpuckett@gmail.com>
2018-01-21 17:06:32 -0800

    initilizing structs

---
+++ structs.go
@@ -0,0 +1,32 @@
+// custom data types
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    type cat struct {
+        name string
+        color string
+        attackRating float64
+    }
+
+    // ways to declare and intialize variables of type cat
+    var milo cat
+    milo.name = "Milo"
+    milo.color = "Orange"
+    milo.attackRating = 8.7
+    
+    eva := new(cat) // this one gives us a pointer
+    eva = &cat{ "Eva", "Gray", 3.6}
+    
+    oreo := cat{
+        name: "Oreo",
+        color: "black and white",
+        attackRating: 7.2,
+    }
+
+    fmt.Printf("milo: %v,\n eva: %v,\n oreo: %v", milo, *eva, oreo)
+}
